229f990c428b837dbf27ff74730caa3d3a88cf39
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-07-30 16:29:25 +0530

    feat: adds credendtial based signin and signout

--- auth.ts
+++ auth.ts
@@ -1,17 +1,46 @@
-import NextAuth from "next-auth"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import client from "./lib/db"
-import Resend from "next-auth/providers/resend"
-import Google from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import client from "./lib/db";
+import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import type { Provider } from "next-auth/providers";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+const providers: Provider[] = [
+  Credentials({
+    credentials: { password: { label: "Password", type: "password" } },
+    authorize(c) {
+      if (c.password !== "password") return null;
+      return {
+        id: "test",
+        name: "Test User",
+        email: "test@example.com",
+      };
+    },
+  }),
+  Google,
+  Resend({
+    apiKey: process.env.AUTH_RESEND_KEY,
+    from: process.env.EMAIL_FROM || "onboarding@resend.dev",
+  }),
+];
+
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === "function") {
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
+    } else {
+      return { id: provider.id, name: provider.name };
+    }
+  })
+  .filter((provider) => provider.id !== "credentials");
+
+export const  { handlers, auth, signIn, signOut } = NextAuth({
   adapter: MongoDBAdapter(client),
-  providers: [
-    Google,
-    Resend({
-      apiKey: process.env.AUTH_RESEND_KEY,
-      from: process.env.EMAIL_FROM || "onboarding@resend.dev",
-    }),
-  ],
+  providers,
   secret: process.env.AUTH_SECRET || process.env.BETTER_AUTH_SECRET,
-})
+  pages: {
+    signIn: "/signin",
+  },
+});
